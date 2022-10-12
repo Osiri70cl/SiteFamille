d057f0121ee9f955740e951d42df22b6a88f522d
Remi Pince <remi.pince@outlook.com>
2022-10-12 17:27:29 +0200

    modification nom de classes et scss pour les cards et banner

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Mainbanner = ({ image, title }) => {
   return (
-    <div className="thumb">
-      <img src={image} alt="" className="thumb__img" />
-      <div className="thumb__overlay"></div>
-      <h2 className="thumb__title">
+    <div className="banner">
+      <img src={image} alt="" className="banner_img" />
+      <div className="banner__overlay"></div>
+      <h2 className="banner__title">
         Bienvenue sur notre site de loisirs familliaux
       </h2>
     </div>
