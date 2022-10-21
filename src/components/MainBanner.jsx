b445223e08d58b276a7cc5eb9d248e42122d7dd2
Remi Pince <remi.pince@outlook.com>
2022-10-21 11:52:10 +0200

    début e mise en place de la section frigo

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Mainbanner = ({ image, title }) => {
   return (
     <div className="banner">
-      <img src={image} alt="" className="banner_img" />
+      <img src={image} alt="" className="banner__img" />
       <div className="banner__overlay"></div>
       <h2 className="banner__title">
         Bienvenue sur notre site de loisirs familliaux
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Mainbanner from "../components/Mainbanner";
 import Homecards from "../components/Homecards";
-import image from "../assets/img/photoMain.png";
+import image from "../assets/img/PhotoMain.jpg";
 
 const Home = () => {
   return (
