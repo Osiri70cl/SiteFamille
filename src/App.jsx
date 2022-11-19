5ce6578739414c41de45dff1db0f27ac96736a9b
Remi Pince <remi.pince@outlook.com>
2022-11-19 11:56:19 +0100

    supp element

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Error from "./pages/Error";
 import Contact from "./pages/Contact";
 import Connection from "./pages/Connection";
 import Inscription from "./pages/Inscription";
